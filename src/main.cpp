128f60f8445f7294452d576cfaa14f649d6e500c
AlvaroMarques <sc.poi458@gmail.com>
2020-11-03 17:25:35 -0300

    Moving files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "gameView.h"
 #include "characterModel.h"
+#include "sdl_keyboard_handler.h"
 
 int main(){
 	int x[] = {255,255,255};
 	PersonagemView p(40,40, -1, x);
 
 	GameView gameView(p);
-	SDL_Keyboard_Handler keyboardHandler();
+	SDL_Keyboard_Handler keyboardHandler;
 	while (gameView.draw() == 0){
 			
 	}
