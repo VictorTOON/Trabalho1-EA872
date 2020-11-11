05459115daa532478cf89c54c4ac29caa6d9bc8e
AlvaroMarques <sc.poi458@gmail.com>
2020-11-11 10:57:13 -0300

    reorganizando arquivos

--- include/characterModel.h
+++ include/characterModel.h
@@ -1,19 +1,19 @@
+#pragma once
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+#include "sdl_keyboard_handler.h"
+#include <cmath>
+#include "bodyModel.h"
+#include "axeController.h"
 
-class Personagem{
-    private:
-        int x, y, health;
-        float teta;
-    public:
-        Personagem();
-        int get_x();
-        void set_x(int);
-        int get_y();
-        void set_y(int);
-        float get_teta();
-        void set_teta(float);
-        int get_health();
-        void set_health(int);
+class PersonagemModel: public CorpoModel{
+	private:
+		std::vector<AxeController> axeControllers;
+		unsigned int tempo;
+		unsigned int tempoComparado;
+	public:
+		PersonagemModel(int x, int y, int h, int w, float teta);
+		RetornoHandle handle_keyboard(int);
+		bool isDead();
 };
--- include/gameView.h
+++ include/gameView.h
@@ -2,13 +2,28 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iomanip>
+#include "characterView.h"
+#include "zombieView.h"
+#include "axeView.h"
+#include <memory>
+#include <vector>
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 900
+
 class GameView{
 	private:
-		PersonagemView personagemView;
+		SDL_Texture *backgroundTexture;
+		SDL_Window *window;
+		SDL_Renderer* renderer;
+		SDL_Event event;
 	public:
 		GameView();
 		~GameView();
-		drawGame();
-        setBackground();
-
-}
+		int draw();
+		void finishDraw();
+		void setBackground();
+		void addAxe(std::shared_ptr<AxeView> axe);
+		void addZumbi(std::shared_ptr<ZumbiView> z);
+		void addPersonagem(std::shared_ptr<PersonagemView> p);
+		void drawBackground();
+};
