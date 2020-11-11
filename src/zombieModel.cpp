3bd5757853e2d4998bf8b923b6d4311cacff6cdd
Victor Toon <araujo.victor200@gmail.com>
2020-11-11 14:22:28 -0300

    Arrumando o Aqui

--- src/zombieModel.cpp
+++ src/zombieModel.cpp
@@ -18,7 +18,6 @@ ZumbiFollowReturn ZumbiModel::follow(std::shared_ptr<PersonagemModel> p){
 		this->set_y(this->get_y() - PASSO * CONST_SPIN);
 	}
 	if (SDL_HasIntersection(this->get_rect(), p->get_rect()) == SDL_TRUE){
-		std::cout<<"Aqui"<<std::endl;
 		p->set_health(p->get_health()-1);
 		return CollisionPersonagem;
 	}
