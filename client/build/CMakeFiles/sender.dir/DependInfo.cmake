58dbd3c05a23f4487e1b9198628eca38cc94e39e
Victor Toon <araujo.victor200@gmail.com>
2021-01-13 12:18:08 -0300

    culpa do 16

---
+++ client/build/CMakeFiles/sender.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/victortoon/HelloTeam/Trabalho1-EA872/client/src/sender.cpp" "/Users/victortoon/HelloTeam/Trabalho1-EA872/client/build/CMakeFiles/sender.dir/src/sender.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/SDL2"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/victortoon/HelloTeam/Trabalho1-EA872/client/build/CMakeFiles/clientController.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
