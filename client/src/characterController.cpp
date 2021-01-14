8e73249a000a75e539a9409ee68d3c7879c11f30
Victor Toon <araujo.victor200@gmail.com>
2021-01-14 18:28:18 -0300

    Apagando comentários indesejados

--- client/src/characterController.cpp
+++ client/src/characterController.cpp
@@ -38,38 +38,26 @@ nlohmann::json PersonagemController::getStateJson(){
 
 void PersonagemController::readStateJson(nlohmann::json stateJson) {
 	this->model->readStateJson(stateJson["model"]);
-    std::cout<<"Boletop"<<stateJson<<std::endl;
 	for (auto& machado: this->axeControllers){
         if (!stateJson[MAP_KEY_AXES].is_null()){
-            std::cout<<"Boletop "<<"44 "<<machado.first<<std::endl;
             if (stateJson[MAP_KEY_AXES].count(machado.first) > 0){
-                std::cout<<"Boletop caiu no if"<< std::endl;
 			    this->axeControllers.find(machado.first)->second.readStateJson(stateJson[MAP_KEY_AXES][machado.first]);	
 		    } else {
-                std::cout<<"Boletop caiu no else"<< std::endl;
 			    this->axeControllers.erase(machado.first);
 			    continue;
 		        }
 		    stateJson[MAP_KEY_AXES].erase(machado.first);
             }
-            std::cout<<"Boletop "<<"53"<<stateJson<<std::endl;
 	}
-    std::cout<<"BoletopMEIO"<<stateJson[MAP_KEY_AXES]<<std::endl;
     if (!stateJson[MAP_KEY_AXES].is_null()){
         std::unordered_map<std::string, nlohmann::json> axesJson = stateJson[MAP_KEY_AXES];
-        std::cout<<"Boletop "<<stateJson[MAP_KEY_AXES]<<std::endl;
         for (auto& pair_machadoJson: axesJson){
             nlohmann::json machadoJson = (nlohmann::json) pair_machadoJson.second;
             std::string machadoId = pair_machadoJson.first;
-            //AxeController axeControllerr(pair_machadoJson.second["x"], pair_machadoJson.second["y"], pair_machadoJson.second["teta"]);
-            std::cout<<"Boletop"<<machadoJson["model"]["base"]["x"]<<std::endl;
-            std::cout<<"Boletop"<<machadoJson<<std::endl;
             AxeController axeController(machadoJson["model"]["base"]["x"], machadoJson["model"]["base"]["y"], machadoJson["model"]["base"]["teta"]);
             axeController.getView()->set_render(this->view->get_render());
             this->axeControllers.insert(std::make_pair(machadoId, axeController));
 	    }
-        std::cout<<"Boletop"<<"terminou o for"<<std::endl;
     }
-    std::cout<<"Boletop"<<"terminou a funcao toda nessa porra"<<std::endl;
 }
 
